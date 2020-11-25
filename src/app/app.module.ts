b9cb25670eb14338f3b682e7f9483621d9cec63c
Vipul Daripkar <vipuldaripkar@gmail.com>
2020-11-25 08:21:28 +0530

    Added reactive login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -44,6 +44,7 @@ import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.com
     AngularMaterialModule,
     FlexLayoutModule,
     FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
